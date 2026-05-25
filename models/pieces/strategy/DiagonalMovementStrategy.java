7f3a1fb2ca94bce1bfd969a46e13a585796f96c7
Reema Dhanwani <reemadhanwani1234@gmail.com>
2026-05-26 01:50:16 +0530

    Added diagonal movement strategy

--- models/pieces/strategy/DiagonalMovementStrategy.java
+++ models/pieces/strategy/DiagonalMovementStrategy.java
@@ -7,8 +7,29 @@ public class DiagonalMovementStrategy implements MovementStrategy{
 
     @Override
     public boolean canMove(Cell start, Cell end, ChessBoard board) {
-        int distX = Math.abs(start.getHorizontalDist(end));
-        int distY = Math.abs(start.getVerticalDist(end));
-        return distY == distX;
+//        start and end cannot be same
+       if(start.getX() == end.getX() && start.getY() == end.getY()) return false;
+
+       int startRow = start.getX();
+       int endRow = end.getX();
+
+       int startCol = start.getY();
+       int endCol = end.getY();
+
+       if(Math.abs(startRow-endRow) != Math.abs(startCol - endCol)) return false;
+
+       int rowStep = (startRow > endRow) ? -1 : 1;
+       int colStep = (startCol > endCol)?-1:1;
+
+       int currRow = startRow + rowStep;
+       int currCol = startCol + colStep;
+
+       while(currRow != endRow && currCol != endCol) {
+           if(board.getBoard().get(currRow).get(currCol).hasPiece()) return false;
+           currRow+=rowStep;
+           currCol+=colStep;
+       }
+
+       return !end.hasPiece() || end.getPiece().get().getColor() != start.getPiece().get().getColor();
     }
 }
