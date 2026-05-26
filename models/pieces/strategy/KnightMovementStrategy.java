1a3ef608ebdff726636ac0be9e385c059d787b48
Reema Dhanwani <reemadhanwani1234@gmail.com>
2026-05-26 22:45:52 +0530

    Added knight movement strategy

--- models/pieces/strategy/KnightMovementStrategy.java
+++ models/pieces/strategy/KnightMovementStrategy.java
@@ -7,6 +7,16 @@ public class KnightMovementStrategy implements MovementStrategy{
 
     @Override
     public boolean canMove(Cell start, Cell end, ChessBoard board) {
-        return false;
+        if(start.getX() == end.getX() || start.getY() == end.getY()) return false;
+
+
+        int rowDiff = Math.abs(start.getX() - end.getX());
+        int colDiff = Math.abs(start.getY() - end.getY());
+
+        boolean isValid = (rowDiff == 1 && colDiff == 2) || (colDiff == 1 && rowDiff == 2);
+
+        if(!isValid) return false;
+
+        return !end.hasPiece() || end.getPiece().get().getColor() != start.getPiece().get().getColor();
     }
 }
